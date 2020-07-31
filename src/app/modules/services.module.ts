a3ed0b81903935587b04d829159f288144f87a7d
Tanmay Awasthi <tanmayawasthi105@gmail.com>
2020-07-31 12:27:04 +0530

    Individual services component

--- src/app/modules/services.module.ts
+++ src/app/modules/services.module.ts
@@ -1,29 +1,11 @@
 import { NgModule } from "@angular/core";
 import { HomeComponent } from "./home/home.component";
-import { BusinessComponent } from "./business/business.component";
-import { EducationComponent } from "./education/education.component";
-import { KnowyourselfComponent } from "./knowyourself/knowyourself.component";
-import { CareerComponent } from "./career/career.component";
-import { LoveComponent } from "./love/love.component";
-import { HealthComponent } from "./health/health.component";
-import { MarriageComponent } from "./marriage/marriage.component";
-import { HoroscopeComponent } from "./horoscope/horoscope.component";
-import { RemediesComponent } from "./remedies/remedies.component";
 import { CommonModule } from "@angular/common";
 import { SharedModule } from "../common/shared.module";
 
 @NgModule({
     declarations: [
-        HomeComponent,
-        BusinessComponent,
-        EducationComponent,
-        KnowyourselfComponent,
-        CareerComponent,
-        LoveComponent,
-        HealthComponent,
-        MarriageComponent,
-        HoroscopeComponent,
-        RemediesComponent
+        HomeComponent
     ],
     imports: [
         CommonModule,
