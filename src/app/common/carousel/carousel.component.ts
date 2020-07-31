c6ae278fd873a98563578ec095c448dbe14c1257
Tanmay Awasthi <tanmayawasthi105@gmail.com>
2020-07-31 16:24:09 +0530

    Provision to add content with scheme

--- src/app/common/carousel/carousel.component.ts
+++ src/app/common/carousel/carousel.component.ts
@@ -1,17 +1,30 @@
 import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-carousel',
-  templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.scss']
+    selector: 'app-carousel',
+    templateUrl: './carousel.component.html',
+    styleUrls: ['./carousel.component.scss']
 })
 export class CarouselComponent implements OnInit {
+    @Input("models") models: [];
+    @Input("size") size;
 
-  @Input("models") models: [];
+    constructor() {
+    }
 
-  constructor() { }
+    ngOnInit(): void {
+    }
 
-  ngOnInit(): void {
-  }
+    getImageUrl(model) {
+        return {'background-image': 'url(' + 'assets/images/modules/' + model.image + ')'};
+    }
+
+    getTextScheme(model) {
+        return (model.scheme === 'dark') ? {'color': 'black'} : {'color': 'white'};
+    }
+
+    getBackgroundScheme(model) {
+        return (model.scheme === 'dark') ? {'background-color': 'black'} : {'background-color': 'white'};
+    }
 
 }
