95cbab9453823770461daab84e80016425ad397a
Tanmay Awasthi <tanmayawasthi105@gmail.com>
2020-11-20 13:34:07 +0530

    Calling email API based on the running environment

--- src/app/services/email.service.ts
+++ src/app/services/email.service.ts
@@ -1,19 +1,20 @@
 import { Injectable } from '@angular/core';
 import { User } from "../model/user.model";
 import { HttpClient } from "@angular/common/http";
+import { environment } from '../../environments/environment';
 
 @Injectable({
     providedIn: 'root'
 })
 export class EmailService {
-    //TODO: Make this generic based on the environment
-    private baseUrl = 'http://localhost:8000';
+    private baseUrl = (environment.production) ? 'https://astroproject-server.herokuapp.com' : 'http://localhost:8000';
     private emailAPI = '/api/sendEmail';
 
     constructor(private http: HttpClient) {
     }
 
     send(user: User): Promise<any>{
+        console.log(environment);
         return this.http.post(this.baseUrl + this.emailAPI, user).toPromise();
     }
 }
