0daf56563b184be0ed88ef39a36d9caed293cd06
Tanmay Awasthi <tanmayawasthi105@gmail.com>
2021-02-21 21:26:20 +0530

    Fix build issue

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -16,6 +16,7 @@ export class HomeComponent implements OnInit {
     tileContent: any;
     lang: string;
     common: {
+        getStarted: any;
         knowMore: "",
         whyChooseUs: ""
     };
